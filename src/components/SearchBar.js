eed04373886e0ad3ffeb739d8c70246fc386f593
Har Dhillon <har7173@gmail.com>
2021-02-12 12:35:09 +0400

    making the input a controlled elemnt

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  state = { term: '' };
 
   render () {
     return (
@@ -13,7 +11,11 @@ class SearchBar extends React.Component {
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type='text'onChange={this.onInputChange} />
+            <input 
+            type='text'
+            // making this a controlled element
+            value={this.state.term} 
+            onChange={(e) => this.setState({ term: e.target.value })} />
           </div>
         </form>
       </div>
