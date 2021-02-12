ddc83936c109932f3d8acee69e11e25a4bd3311f
Har Dhillon <har7173@gmail.com>
2021-02-12 11:55:07 +0400

    adding an on change event listener

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
   render () {
     return (
 
@@ -8,8 +13,7 @@ class SearchBar extends React.Component {
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type='text'>
-            </input>
+            <input type='text'onChange={this.onInputChange} />
           </div>
         </form>
       </div>
