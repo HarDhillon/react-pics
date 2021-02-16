fcf562c6259ad46edd2d6fd9f3f73fcb8c89cc6d
Har Dhillon <har7173@gmail.com>
2021-02-16 16:51:36 +0400

    added unique key and alt to img tag

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const ImageList = (props) => {
-  const images = props.images.map(image => {
-    return <img src={image.urls.regular} />
+  const images = props.images.map(({ description, id, urls }) => {
+    return <img alt={description} key={id} src={urls.regular} />
   });
-
+  console.log(images)
   return <div>{images}</div>;
 };
 
