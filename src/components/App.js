0888af0d3ba1769bd60a7d37e0fd99d81b74141b
Har Dhillon <har7173@gmail.com>
2021-02-16 15:00:27 +0400

    using asynch to wait and store response from unsplash search

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term)
+  // instead of .then we can use async
+  async onSearchSubmit(term) {
+    // using axios which is like fetch, but better
+    // use await instead of .then
+    const response = await axios
+    .get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID 2b98c1afb0aed3b3d94a1866bdc3ac013d21a0c86d236a0fee32355c331c0296'
+      }
+    });
+
+    console.log(response.data.results)
   }
 
   render() {
